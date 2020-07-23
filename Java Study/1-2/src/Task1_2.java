cba2d61980c3fab21cfc78788109f5158a58a1da
aoyagi-t <67412487+aoyagi-t@users.noreply.github.com>
2020-07-23 14:05:26 +0900

    Task1_2 課題
    カリキュラム「配列」の課題提出

--- Java Study/1-2/src/Task1_2.java
+++ Java Study/1-2/src/Task1_2.java
@@ -23,7 +23,7 @@ public class Task1_2 {
          */
     	String[] strArray = {"りんご","ぶどう","もも"};
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
-    	System.out.println(strArray[1]);
+    	System.out.println(strArray[2]);
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
          *  [int型の変数 intArrayの配列に、10,20,30,40,50の値でそれぞれ初期化した。]
          */
